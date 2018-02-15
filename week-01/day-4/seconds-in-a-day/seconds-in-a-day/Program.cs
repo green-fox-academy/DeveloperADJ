d1b4c98f72f25d08d0189022a838d5eacc6dd759
unknown <developeradj@gmail.com>
2018-02-15 14:09:07 +0100

    Added the seconds in a day exercise

---
+++ week-01/day-4/seconds-in-a-day/seconds-in-a-day/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented by the variables
+
+            int totalSecondsPerDay = 24 * 60 * 60;
+
+            int currentTimeInSeconds = currentSeconds + (currentMinutes * 60) + (currentHours * 60 * 60);
+
+            Console.WriteLine(totalSecondsPerDay - currentTimeInSeconds + " seconds left in the day.");
+            Console.ReadLine();
+
+        }
+    }
+}
