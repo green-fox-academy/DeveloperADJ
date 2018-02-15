156743c91dd99d4bb096204fca24931294453080
unknown <developeradj@gmail.com>
2018-02-15 14:33:43 +0100

    Added average-of-input exercise

---
+++ week-01/day-4/average-of-input/average-of-input/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+            Console.Write("Please enter a first number: ");
+            double number = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a second number: ");
+            number += double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a third number: ");
+            number += double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a fourth number: ");
+            number += double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter a fifth number: ");
+            number += double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sum: " + number + ", Average: " + number/5);
+
+            Console.ReadLine();
+
+        }
+    }
+}
