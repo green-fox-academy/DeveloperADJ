6a4cebf0d7801d221d76cf8a0b7b0c52fd7b2e78
unknown <developeradj@gmail.com>
2018-02-15 14:02:17 +0100

    Added Cuboid exercise

---
+++ week-01/day-4/cuboid/cuboid/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+            int sideA = 5;
+            int sideB = 6;
+            int sideC = 7;
+
+            int surfaceArea = (2 * sideA * sideB) + (2 * sideB * sideC) + (2 * sideC * sideA);
+            int volume = sideA * sideB * sideC;
+
+            Console.WriteLine("surfaceArea Area: " + surfaceArea);
+            Console.WriteLine("Volume: " + volume);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
