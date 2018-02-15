e2af2fda828084c1679faf2a17059b87d1858084
unknown <developeradj@gmail.com>
2018-02-15 12:26:37 +0100

    Added 3 more exercises

---
+++ week-01/day-4/define-basic-info/define-basic-info/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable then print their values
+            // Your name as a string
+            string name = "Arno";
+
+            // Your age as an integer
+            int age = 28;
+
+            // Your height in meters as a double
+            double height = 1.82;
+
+            // Whether you are married or not as a boolean
+            bool isMarried = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(isMarried);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-01/day-4/favorite-number/favorite-number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            int favoriteNumber = 777;
+
+            // And print it like this: "My favorite number is: 8"
+            Console.WriteLine("My favorite number is: " + favoriteNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
