7af40cb3f2d770a49bb8b59bd029a6da85ae562e
unknown <developeradj@gmail.com>
2018-02-15 14:38:14 +0100

    Added the OddEven exercise

---
+++ week-01/day-4/OddEven/OddEven/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input,
+            // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+            Console.WriteLine("Please enter a number: ");
+            double number = double.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
