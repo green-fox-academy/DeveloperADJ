695cd9cc3f457f3f4fb566dd1682ee4abca2d6b3
unknown <developeradj@gmail.com>
2018-02-15 16:11:00 +0100

    Added the print-bigger exercise

---
+++ week-01/day-4/print-bigger/print-bigger/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+            Console.WriteLine("Please enter a first number: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("The biggest number is: ");
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else
+            {
+                Console.WriteLine(secondNumber);
+            }
+            
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
