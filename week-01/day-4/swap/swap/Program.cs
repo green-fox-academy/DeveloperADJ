59b7646329a48aba435717cbf16c42c3a16dbc4b
unknown <developeradj@gmail.com>
2018-02-15 12:29:59 +0100

    Edited swap exercise

--- week-01/day-4/swap/swap/Program.cs
+++ week-01/day-4/swap/swap/Program.cs
@@ -9,6 +9,16 @@ namespace GreenFox
             // Swap the values of the variables
             int a = 123;
             int b = 526;
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("A should be 526:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("A should be 526:");
+            Console.WriteLine("b = " + b);
+            Console.ReadLine();
+
         }
     }
 }
