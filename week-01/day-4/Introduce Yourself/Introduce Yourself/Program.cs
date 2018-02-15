c067323f9799c045abb6e1b8ed36e8d010cce89b
unknown <developeradj@gmail.com>
2018-02-15 12:12:41 +0100

    Added 3 more exercises

---
+++ week-01/day-4/Introduce Yourself/Introduce Yourself/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that prints a few details to the terminal window about you
+            // It should print each detail to a new line.
+            //  - Your name
+            //  - Your age
+            //  - Your height in meters (as a decimal fraction)
+            //
+            //  Example output:
+            //  John Doe
+            //  31
+            //  1.87
+            string name = "Arno";
+            int age = 28;
+            float height = 1.82f;
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Height: {0}m", height);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ week-01/day-4/coding-hours/coding-hours/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // An average Green Fox attendee codes 6 hours daily
+            // The semester is 17 weeks long
+            double weeks = 17;
+            double workdays = 5;
+            double hoursPerDay = 6;
+
+            // Print how many hours is spent with coding in a semester by an attendee,
+            // if the attendee only codes on workdays.
+            Console.WriteLine(weeks*workdays*hoursPerDay);
+
+            // Print the percentage of the coding hours in the semester if the average
+            // work hours weekly is 52
+            double workHoursPerWeek = 52;
+            Console.WriteLine((workdays*hoursPerDay)/workHoursPerWeek);
+
+            Console.ReadLine();
+
+        }
+    }
+}
