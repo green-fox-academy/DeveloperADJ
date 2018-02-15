a6024a30b81421f1578f0eb415dff27e007fa952
unknown <developeradj@gmail.com>
2018-02-15 17:12:29 +0100

    Added draw-triangle to the exercises

---
+++ week-01/day-4/draw-triangle/draw-triangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+            Console.WriteLine("Please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            string asterisk = "*";
+            string oneMoreAsterisk = "*";
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(asterisk);
+                asterisk += oneMoreAsterisk;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
