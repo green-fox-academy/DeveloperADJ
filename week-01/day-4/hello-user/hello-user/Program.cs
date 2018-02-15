e196b2ce71ea0c04a01f55aa3e4a80fbbdea8a61
unknown <developeradj@gmail.com>
2018-02-15 14:16:22 +0100

    Added hello-user and mile-to-km-converter

---
+++ week-01/day-4/hello-user/hello-user/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+            Console.Write("Hello, what is your name? ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", username);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ week-01/day-4/mile-to-km-converter/mile-to-km-converter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            Console.WriteLine("Please enter a value in Kilometers here: ");
+            double kilometers = double.Parse(Console.ReadLine());
+            double miles = kilometers * 0.621371192;
+            Console.WriteLine("Thats " + miles + " miles.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
