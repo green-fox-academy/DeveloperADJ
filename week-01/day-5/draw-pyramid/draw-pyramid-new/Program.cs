e5b6d9d4899fcf86a703740348146a9154c2fe87
unknown <developeradj@gmail.com>
2018-02-16 12:01:20 +0100

    Moved and renamed some folders and added exercises

---
+++ week-01/day-5/draw-pyramid/draw-pyramid-new/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+
+            Console.WriteLine("Please enter a number: ");
+            int numberOfAsterisks = int.Parse(Console.ReadLine());
+            int numberOfWhitespaces = numberOfAsterisks;
+            string asterisk = "*";
+            string whitespace = " ";
+
+            for (int i = 0; i < numberOfAsterisks; i++)
+            {
+
+                for (int x = 0; x < numberOfWhitespaces; x++)
+                {
+                    Console.Write(whitespace);
+                }
+
+                numberOfWhitespaces -= 1;
+
+                Console.WriteLine(asterisk);
+
+                asterisk += "**";
+
+            }
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
