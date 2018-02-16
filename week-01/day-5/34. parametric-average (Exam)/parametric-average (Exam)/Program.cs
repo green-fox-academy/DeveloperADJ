7a69edce61b90fd305636040694ddb0fe21eae47
unknown <developeradj@gmail.com>
2018-02-16 14:00:50 +0100

    Added the parametric-average exercise

---
+++ week-01/day-5/34. parametric-average (Exam)/parametric-average (Exam)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            Console.Write("How many numbers would you like to enter? ");
+            int numberOfIntegers = int.Parse(Console.ReadLine());
+            double total = 0;
+
+            for (int i = 0; i < numberOfIntegers; i++)
+            {
+                Console.Write("Please enter a number: ");
+                total += double.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Sum: " + total + ", Average: " + (total/numberOfIntegers));
+
+            Console.ReadLine();
+        }
+    }
+}
