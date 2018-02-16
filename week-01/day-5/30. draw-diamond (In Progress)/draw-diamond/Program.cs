3f53ae655d4644150ee4547710c95f16005f1fa0
unknown <developeradj@gmail.com>
2018-02-16 12:22:32 +0100

    Created projects for multiple (In Progress) exercises

---
+++ week-01/day-5/30. draw-diamond (In Progress)/draw-diamond/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //  *****
+            //   ***
+            //    *
+            //
+            // The diamond should have as many lines as the number was
+
+            Console.WriteLine("Please enter a number: ");
+            int numberOfAsterisks = int.Parse(Console.ReadLine());
+            int numberOfWhitespaces = numberOfAsterisks;
+            string asterisk = "*";
+            string whitespace = " ";
+
+            for (int i = 0; i < numberOfAsterisks; i++)
+            {
+
+                for (int x = 0; x < numberOfWhitespaces; x++)
+                {
+                    Console.Write(whitespace);
+                }
+
+                numberOfWhitespaces -= 1;
+
+                Console.WriteLine(asterisk);
+
+                asterisk += "**";
+
+            }
+
+            numberOfWhitespaces += 1;
+
+            for (int y = 0; y < numberOfWhitespaces; y++)
+            {
+                Console.Write(whitespace);
+
+                numberOfAsterisks -= 1;
+
+                for (int z = 0; z < numberOfAsterisks; z++)
+                {
+
+                }
+
+
+            }
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ week-01/day-5/31. draw-square (In Progress)/draw-square/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+
+            if 
+
+
+        }
+    }
+}
