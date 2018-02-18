c0a9eb3a1d211511ce5e7834882d7224218daffa
unknown <developeradj@gmail.com>
2018-02-18 14:52:45 +0100

    Added the doubling exercise

--- week-02/day-1/doubling/doubling/Program.cs
+++ week-02/day-1/doubling/doubling/Program.cs
@@ -7,9 +7,23 @@ namespace Doubling
         static void Main(string[] args)
         {
             // - Create an integer variable named `ak` and assign the value `123` to it
+            int ak = 123;
+
             // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+
+
+
             // - Print the result of `doubling(ak)`
+            Console.WriteLine(Doubling(ak));
+
+
             Console.ReadLine();
         }
+
+        public static int Doubling(int userNumber)
+        {
+            return userNumber * 2;
+        }
+
     }
 }
