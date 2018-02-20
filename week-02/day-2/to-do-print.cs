167be0b024bfa407d7719cf50398e6295dae45a9
unknown <developeradj@gmail.com>
2018-02-20 10:49:52 +0100

    Added to-do-print to exercises

---
+++ week-02/day-2/to-do-print.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace TodoPrint
+{
+    public class TodoPrint
+    {
+        public static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indention
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+             todoText = todoText.Insert(0, "My todo:\n");
+            todoText = todoText.Insert(todoText.Length, " - Download games\n");
+            todoText = todoText.Insert(todoText.Length, "\t - Diablo");
+            Console.WriteLine(todoText);
+            Console.ReadLine();
+        }
+    }
+}
