7041c0fb473dc004c0dd63fe8967f7c2c14824b5
unknown <developeradj@gmail.com>
2018-02-22 17:51:23 +0100

    Edited triangles

--- week-02/day-4/triangles(in-progress).cs
+++ week-02/day-4/triangles(in-progress).cs
@@ -14,44 +14,64 @@ namespace drawing
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
 
-            //Draw a perfect triangle
-
-            //(x + distance * Math.Cos(angle + Math.PI / 3))
-
             int angle = 0;
-            int distance = -200;
+           
+
+            DrawTrianglesWithWidth(-50);
+
+            void DrawTrianglesWithWidth(int triangleSize)
+            {
+                //Where the triangle starts drawing
+                var startPoint = new Point((canvas.Width / 2) - triangleSize / 2, (canvas.Height / 2) - triangleSize / 2);
+
+                //Right surface
+                var point1X = startPoint.X + triangleSize * Math.Cos(angle + Math.PI / 3);
+                var point1Y = startPoint.Y + triangleSize * Math.Sin(angle + Math.PI / 3);
+
+                var point1 = new Point(point1X, point1Y);
+
+                //Left surface
+                var point2X = startPoint.X + triangleSize * Math.Cos(angle);
+                var point2Y = startPoint.Y + triangleSize * Math.Sin(angle);
+                var point2 = new Point(point2X, point2Y);
+
+                foxDraw.DrawLine(startPoint, point1);
+                foxDraw.DrawLine(point1, point2);
+                foxDraw.DrawLine(point2, startPoint);
+
+                for (int i = 1; i < 5; i++)
+                {
+
+                    int shift = triangleSize * i;
+
+                    //Create another loop //
+
+                    //Shift the X axis
+                    foxDraw.DrawLine(new Point(startPoint.X + shift, startPoint.Y), new Point(point1.X + shift, point1.Y));
+                    foxDraw.DrawLine(new Point(point1.X + shift, point1.Y), new Point(point2.X + shift, point2.Y));
+                    foxDraw.DrawLine(new Point(point2.X + shift, point2.Y), new Point(startPoint.X + shift, startPoint.Y));
+                    //
+
+                    //Shift the Y axis
+   
+                }
+
+                for (int i = 1; i < 5; i++)
+                {
+
+                    int shift = triangleSize * i;
+
+                    foxDraw.DrawLine(new Point(startPoint.X, startPoint.Y + shift), new Point(point1.X, point1.Y + shift));
+                    foxDraw.DrawLine(new Point(point1.X, point1.Y + shift), new Point(point2.X, point2.Y + shift));
+                    foxDraw.DrawLine(new Point(point2.X, point2.Y + shift), new Point(startPoint.X, startPoint.Y + shift));
+
+                }
 
 
-            //Where the triangle starts drawing
-            var startPoint = new Point((canvas.Width/2)-distance/2, (canvas.Height/2)-distance/2);
-
-            //Top to right
-            var point1X = startPoint.X + distance * Math.Cos(angle + Math.PI / 3);
-            var point1Y = startPoint.Y + distance * Math.Sin(angle + Math.PI / 3);
-
-            var point1 = new Point(point1X, point1Y);
-
-            foxDraw.DrawLine(startPoint, point1);
-
-            //Top to left
-            var point2X = startPoint.X + distance * Math.Cos(angle);
-            var point2Y = startPoint.Y + distance * Math.Sin(angle);
-            var point2 = new Point(point2X, point2Y);
-
-            foxDraw.DrawLine(point1, point2);
-
-            foxDraw.DrawLine(point2, startPoint);
-
-
-
-
-
+            }
 
 
         }
-
-
-
 
     }
 }
