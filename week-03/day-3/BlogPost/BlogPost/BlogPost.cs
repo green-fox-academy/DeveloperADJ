47862ce800f7c09d3422d5113e86b58b2ceca554
unknown <developeradj@gmail.com>
2018-02-28 11:37:50 +0100

    Added the blog-post exercise

---
+++ week-03/day-3/BlogPost/BlogPost/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogPost
+{
+    class BlogPost
+    {
+        string AuthorName;
+        string Title;
+        string Text;
+        string PublicationDate;
+
+        public BlogPost(string AuthorName, string Title, string Text, string PublicationDate)
+        {
+            this.AuthorName = AuthorName;
+            this.Title = Title;
+            this.Text = Text;
+            this.PublicationDate = PublicationDate;
+        }
+    }
+}
