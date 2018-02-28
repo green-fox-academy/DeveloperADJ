2dbe66a57258ed167195f7240fdceb89e1e88b76
unknown <developeradj@gmail.com>
2018-02-28 16:31:13 +0100

    Added the SharpieSet exercise to the list

---
+++ week-03/day-3/SharpieSet/SharpieSet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SharpieSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Sharpie Set
+            Reuse your Sharpie class
+            Create SharpieSet class
+            it contains a list of Sharpie
+            Add method CountUsable() -> sharpie is usable if it has ink in it
+            Add method RemoveTrash() -> removes all unusable sharpies*/
+
+            SharpieSet.AddSharpieToList(new Sharpie("pink", 2));
+            SharpieSet.AddSharpieToList(new Sharpie("red", 2));
+            SharpieSet.AddSharpieToList(new Sharpie("blue", 2));
+            SharpieSet.AddSharpieToList(new Sharpie("yellow", 2, 0));
+
+            Console.WriteLine($"You have {SharpieSet.Sharpies.Count} sharpies.");
+
+            SharpieSet.CountUsable();
+
+            SharpieSet.RemoveTrash();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ week-03/day-3/SharpieSet/SharpieSet/SharpieSet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpieSet
+{
+    class SharpieSet
+    {
+
+        public static List<Sharpie> Sharpies = new List<Sharpie>();
+        public static List<Sharpie> UsableSharpies = new List<Sharpie>();
+
+        public static void AddSharpieToList(Sharpie sharpie)
+        {
+            Sharpies.Add(sharpie);
+        }
+
+        public static int CountUsable()
+        {
+            int count = 0;
+
+            foreach (var sharpie in Sharpies)
+            {
+                if (sharpie.inkAmount > 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"There are {count} usable sharpies left.");
+            return count;
+        }
+
+        public static void RemoveTrash()
+        {
+            int amountRemoved = 0;
+            for (int i = 0; i < Sharpies.Count; i++)
+            {
+                
+                if (Sharpies[i].inkAmount <= 0)
+                {
+                    amountRemoved++;
+                    Sharpies.RemoveAt(i);
+                }
+            }
+
+            Console.WriteLine($"Removed {amountRemoved} from the set.");
+        }
+
+    }
+}
