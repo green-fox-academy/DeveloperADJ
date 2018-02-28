61231b518dcff6c2514bdd9f9a5d2b96bf01cad8
unknown <developeradj@gmail.com>
2018-02-28 15:34:04 +0100

    Added the Sharpie and Counter(in-progress) exercises

---
+++ week-03/day-3/Sharpie/Sharpie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sharpie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Sharpie
+            Create Sharpie class
+            We should know about each sharpie their Color (which should be a string), Width (which will be a floating point number), InkAmount (another floating point number)
+            When creating one, we need to specify the Color and the Width
+            Every sharpie is created with a default 100 as InkAmount
+            We can Use() the sharpie objects
+            which decreases inkAmount*/
+
+            Sharpie PinkSharpie = new Sharpie("pink", 2);
+
+
+            PinkSharpie.Use();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-03/day-3/Sharpie/Sharpie/Sharpie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Sharpie
+{
+    class Sharpie
+    {
+        string sharpieColor;
+        double tipWidth;
+        double inkAmount;
+
+        public Sharpie(string sharpieColor, double tipWidth, double inkAmount = 100)
+        {
+            this.sharpieColor = sharpieColor;
+            this.tipWidth = tipWidth;
+            this.inkAmount = inkAmount;
+        }
+
+        public void Use()
+        {
+            inkAmount -= tipWidth;
+            Console.WriteLine($"You marked with your {sharpieColor} sharpie, you have {inkAmount} ink left.");
+        }
+
+
+    }
+}
