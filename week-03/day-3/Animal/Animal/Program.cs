6ad970cd56dc468710d18ede6290ceb282f744ce
unknown <developeradj@gmail.com>
2018-02-28 15:23:46 +0100

    Added the Animal exercise

---
+++ week-03/day-3/Animal/Animal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Animal
+            Create an Animal class
+            Every animal has a hunger value, which is a whole number
+            Every animal has a thirst value, which is a whole number
+            when creating a new animal object these values are created with the default 50 value
+            Every animal can eat() which decreases their hunger by one
+            Every animal can drink() which decreases their thirst by one
+            Every animal can play() which increases both by one*/
+
+            Animal Monkey = new Animal();
+
+            Monkey.drink();
+            Monkey.eat();
+            Monkey.play();
+
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
