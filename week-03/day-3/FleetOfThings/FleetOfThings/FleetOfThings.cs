45bedbe8688fe364390779a75e71783664ef3bf7
unknown <developeradj@gmail.com>
2018-02-28 15:15:38 +0100

    Added the FleetOfThings exercise

---
+++ week-03/day-3/FleetOfThings/FleetOfThings/FleetOfThings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FleetOfThings
+{
+    public class FleetOfThings
+    {
+        public static void Main(string[] args)
+        {
+            var fleet = new Fleet();
+
+            var Milk = new Thing("Milk", false);
+            fleet.Add(Milk);
+
+            var RemoveObstacles = new Thing("Remove the obstacles", false);
+            fleet.Add(RemoveObstacles);
+
+            var StandUp = new Thing("Stand up", true);
+            fleet.Add(StandUp);
+
+            var EatLunch = new Thing("Eat lunch", true);
+            fleet.Add(EatLunch);
+
+            var DoMath = new Thing("Do math", false);
+            fleet.Add(DoMath);
+
+            var TestAgain = new Thing("Test Again", true);
+            fleet.Add(TestAgain);
+
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
+
+
+            for (int i = 0; i < Fleet.Things.Count; i++)
+            {
+                Console.Write($"{i+1}. ");
+
+                if (Fleet.Things[i].Completed == true)
+                {
+                    Console.Write("[x] ");
+                }
+                else
+                {
+                    Console.Write("[ ] ");
+                }
+                Console.Write(Fleet.Things[i].Name + "\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-03/day-3/FleetOfThings/FleetOfThings/Thing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FleetOfThings
+{
+    public class Thing
+    {
+        public string Name;
+        public bool Completed;
+
+        public Thing(string Name, bool Completed)
+        {
+            this.Name = Name;
+            this.Completed = Completed;
+        }
+
+        public Thing(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Complete()
+        {
+            this.Completed = true;
+        }
+
+    }
+}
