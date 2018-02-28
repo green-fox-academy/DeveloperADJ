ebc52c7c2682055590cb940e30bb3efd266c305b
unknown <developeradj@gmail.com>
2018-02-28 14:19:17 +0100

    Added the Dominoes(in-progress) and Dice exercises

---
+++ week-03/day-3/Dice/Dice/Dice.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+
+namespace Dice
+{
+    public class Dice
+    {
+        //    You have a Dice class which has 6 dice
+        //    You can roll all of them with Roll()
+        //    Check the current rolled numbers with GetCurrent()
+        //    You can reroll with Reroll()
+        //    Your task is to get where all dice is a 6
+        static Random RandomValue;
+        static int[] Dices = new int[6];
+
+        public int[] Roll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 7);
+            }
+            return Dices;
+        }
+
+        public int[] GetCurrent()
+        {
+            return Dices;
+        }
+
+        public int GetCurrent(int i)
+        {
+            return Dices[i];
+        }
+
+        public void Reroll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 7);
+            }
+        }
+
+        public void Reroll(int k)
+        {
+            Dices[k] = new Random().Next(1, 7);
+        }
+
+        public void RollTillSix()
+        {
+            int[] ArrayOfSixes = new int[6];
+
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Roll();
+                while (Dices[i] != 6)
+                {
+                    Reroll();
+                    Console.WriteLine("Dice Roll: " + Dices[i]);
+                }
+
+                if (Dices[i] == 6)
+                {
+                    ArrayOfSixes[i] = Dices[i];
+                    Console.WriteLine("Added 6 to the Array");
+                }
+            }
+
+            StringBuilder ArrayOfSixesContents = new StringBuilder();
+
+            ArrayOfSixesContents.Append("ArrayOfSixes: [");
+            for (int i = 0; i < ArrayOfSixes.Length; i++)
+            {
+                ArrayOfSixesContents.Append(ArrayOfSixes[i]);
+
+                if (i < ArrayOfSixes.Length-1)
+                {
+                    ArrayOfSixesContents.Append(", ");
+                }
+            }
+            ArrayOfSixesContents.Append("]");
+
+            Console.WriteLine(ArrayOfSixesContents);
+            Console.ReadLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            RandomValue = new Random();
+            Dice myDice = new Dice();
+
+            myDice.RollTillSix();
+        }
+    }
+}
---
+++ week-03/day-3/dominoes/Dominoes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domino
+{
+    public class Dominoes
+    {
+        public static void Main(string[] args)
+        {
+            List<Domino> dominoes = InitializeDominoes();
+            // You have the list of Dominoes
+            // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+            // Create a function to write the dominous to the console in the following format
+            // eg: [2, 4], [4, 3], [3, 5] ...
+            CompareDominoes(InitializeDominoes());
+            Console.ReadLine();
+        }
+
+        public static void CompareDominoes(List<Domino> listOfDominoes)
+        {
+            for (int i = 0; i < listOfDominoes.Count; i++)
+            {
+                Console.WriteLine(ArePairs(listOfDominoes, i)); 
+            }
+        }
+
+        public static bool ArePairs(List<Domino> listOfDominoes, int i)
+        {
+            if (i+1 < listOfDominoes.Count)
+            {
+                int firstDominoRightNumber = listOfDominoes[i].GetValues()[1];
+                int secondDominoLeftNumber = listOfDominoes[i + 1].GetValues()[0];
+
+                return firstDominoRightNumber == secondDominoLeftNumber;
+            }
+            else
+            {
+                return false;
+            } 
+        }
+
+        public static List<Domino> InitializeDominoes()
+        {
+            var dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+            return dominoes;
+        }
+    }
+}
