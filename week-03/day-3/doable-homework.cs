0d25bce31e483347f0583c160f97a0ec2fa9f6d6
unknown <developeradj@gmail.com>
2018-02-28 11:04:57 +0100

    Added the day-3 folder and the doable-homework exercise

---
+++ week-03/day-3/doable-homework.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace GreenFox
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            /*Doable homework
+            Create 5 trees
+            Store the data of them in variables in your program
+            for every tree the program should store its'
+            type
+            leaf color
+            age
+            sex
+            you can use just variables, or lists and/or maps*/
+
+            string[] type = {"palm", "pine", "apple", "oak", "file"  };
+            Color[] color = {Color.Green, Color.ForestGreen, Color.GreenYellow, Color.DarkOliveGreen, Color.White };
+            int[] age = {20, 50, 15, 30, 1};
+            char[] sex = { 'M', 'F', 'M', 'F', 'F' };
+
+        }
+
+
+    }
+}
