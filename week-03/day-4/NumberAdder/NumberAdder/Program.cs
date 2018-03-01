54d441cf9a6170fcf8a5ad8bae6e8f74b2a4d19f
unknown <developeradj@gmail.com>
2018-03-01 10:55:58 +0100

    Added the NumberAdder exercise

---
+++ week-03/day-4/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(numberAdder(10));
+            Console.Read();
+        }
+
+        public static int numberAdder(int n)
+        {
+            Console.Write(n);
+            if (n == 1)
+            {
+                Console.Write(" = ");
+                return 1;
+            }
+            else
+            {
+                Console.Write(" + ");
+                return n + numberAdder(n - 1);
+            }
+        }
+
+
+    }
+}
