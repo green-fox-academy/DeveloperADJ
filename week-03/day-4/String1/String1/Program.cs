2a58f1940efcb98601b393a039cdbf48f44d41e5
unknown <developeradj@gmail.com>
2018-03-01 13:52:46 +0100

    Added the SumDigit exercise and String1

---
+++ week-03/day-4/String1/String1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace String1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+
+            string sentence = "xxxxxxxxxxxxxxxxxxxxx sdadlkasdkasld xxxxx";
+
+            Console.WriteLine(ReplaceXwithY(sentence));
+            Console.ReadLine();
+        }
+
+        public static string ReplaceXwithY(string sentence, string newSentence = "", int i = 0)
+        {
+            if (i == sentence.Length)
+            {
+                return newSentence;
+            }
+            else
+            {
+                if (sentence[i] == 'x')
+                {
+                    newSentence += 'y';
+                }
+                else
+                {
+                    newSentence += sentence[i];
+                }
+                return ReplaceXwithY(sentence, newSentence, i + 1);
+            }
+        }
+    }
+}
---
+++ week-03/day-4/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumDigits(123));
+            Console.Read();
+        }
+
+        public static int SumDigits(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return (n % 10) + SumDigits(n / 10);
+            }
+        }
+    }
+}
