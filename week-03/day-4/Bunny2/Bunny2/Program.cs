fa620586e5c278c7a3be863f989c1672daa8126f
unknown <developeradj@gmail.com>
2018-03-01 12:12:22 +0100

    Added the Bunny2 exercise

--- week-03/day-4/Bunny2/Bunny2/Program.cs
+++ week-03/day-4/Bunny2/Bunny2/Program.cs
@@ -6,7 +6,34 @@ namespace Bunny2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(countBunnyEars(7));
+            Console.ReadLine();
+        }
+
+        public static int countBunnyEars(int bunnies)
+        {
+            if (bunnies == 1)
+            {
+                if (bunnies % 2 == 0)
+                {
+                    return 3;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+            else
+            {
+                if (bunnies % 2 == 0)
+                {
+                    return 3 + countBunnyEars(bunnies - 1);
+                }
+                else
+                {
+                    return 2 + countBunnyEars(bunnies - 1);
+                }
+            }
         }
     }
 }
