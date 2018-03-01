145bce85da73185eedc4b2d7abe9bb5678711729
unknown <developeradj@gmail.com>
2018-03-01 10:44:10 +0100

    Added the Pirate and Counter exercises and created a folder for day-4

---
+++ week-03/day-3/Pirate/Pirate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pirate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pirate.DrinkSomeRum(3);
+            Pirate.HowsItGoingMate();
+            Pirate.DrinkSomeRum(8);
+            Pirate.HowsItGoingMate();
+
+            Pirate.Die();
+            Pirate.DrinkSomeRum();
+            Pirate.HowsItGoingMate();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-03/day-4/Counter/Counter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Counter(10));
+            Console.ReadLine();
+        }
+
+        public static int Counter(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                Console.Write(n + ", ");
+                return Counter(n - 1);
+            }
+        }
+    }
+}
