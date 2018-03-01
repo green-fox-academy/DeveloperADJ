1b6536de18380dcf1030499312e04ae0605e9030
unknown <developeradj@gmail.com>
2018-03-01 16:32:22 +0100

    Added the Refactorio(in-progress) exercise

---
+++ week-03/day-4/Refactorio(in-progress)/Refactorio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Refactorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a recursive function called `refactorio`
+            // that returns it's input's factorial
+            //input 120 needs to output 5
+            Console.WriteLine(Refactorio(5));
+            Console.ReadLine();
+        }
+
+        public static int Refactorio(int n)
+        {
+            Console.WriteLine(n);
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Refactorio(n - 1);
+            }
+        }
+    }
+}
