b38467941862a66e463028cad3e87e2f9c3e0845
unknown <developeradj@gmail.com>
2018-03-26 14:40:08 +0200

    Completed exercise 8

---
+++ week-06/day-1/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the uppercase characters in a string!
+
+            string myString = "BuuuuULlllIiiii";
+
+            //IEnumerable<char> uppercaseChars = myString.Where(c => char.IsUpper(c));
+
+            IEnumerable<char> uppercaseChars = from ch in myString
+                                               where char.IsUpper(ch)
+                                               select ch;
+
+            foreach (var uppercaseChar in uppercaseChars)
+            {
+                Console.WriteLine(uppercaseChar);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
