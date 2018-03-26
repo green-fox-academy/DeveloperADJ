caa5b2fe228f0d32e1def7a1caa0b9fe7264be56
unknown <developeradj@gmail.com>
2018-03-26 13:30:09 +0200

    Completed the Exercise1 properly

--- week-06/day-1/Exercise1/Exercise1/Program.cs
+++ week-06/day-1/Exercise1/Exercise1/Program.cs
@@ -12,7 +12,11 @@ namespace Exercise1
         {
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
-            IEnumerable<int> evenNumbers = n.Where(x => x % 2 == 0);
+            //IEnumerable<int> evenNumbers = n.Where(x => x % 2 == 0);
+
+            IEnumerable<int> evenNumbers = from number in n
+                                           where number % 2 == 0
+                                           select number;
 
             foreach (var evenNumber in evenNumbers)
             {
