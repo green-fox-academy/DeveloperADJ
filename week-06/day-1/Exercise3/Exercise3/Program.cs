572ada067704d07dfe4e1540db0e1a00eec663f2
unknown <developeradj@gmail.com>
2018-03-26 13:50:11 +0200

    Completed exercise3

---
+++ week-06/day-1/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> squaredNumbers = n.Where(x => x > -1).Select(x => x * x);
+
+            foreach (var squaredNumber in squaredNumbers)
+            {
+                Console.WriteLine(squaredNumber);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
