40c0cd3d575a733f0beb36caead7d1a5f9a6886c
unknown <developeradj@gmail.com>
2018-03-26 13:35:14 +0200

    Added exercise2

---
+++ week-06/day-1/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+        }
+    }
+}
