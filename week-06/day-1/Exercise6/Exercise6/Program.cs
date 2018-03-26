94d78059ffee555a457ed5518952b3b785c5ec61
unknown <developeradj@gmail.com>
2018-03-26 14:26:05 +0200

    Completed exercise 6

---
+++ week-06/day-1/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the frequency of characters in a given string!
+
+            string name = "Allejandro";
+
+            IEnumerable<int> frequencyOfChars = name.ToLower().GroupBy(x => x).Select(x=>x.Count());
+
+            foreach (var chars in frequencyOfChars)
+            {
+                Console.WriteLine(chars);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
