2252499775107088cc29b3c33c9402e9aa71edda
DeveloperADJ <developeradj@gmail.com>
2018-04-03 18:04:29 +0200

    Completed exercise V from the lion king bank exercise

---
+++ week-07/day-3/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; private set; }
+        public string Balance { get; private set; }
+        public string AnimalType { get; private set; }
+        public bool IsKing { get; private set; }
+        public bool IsBadGuy { get; private set; }
+
+        public BankAccount(string name, string balance, string animalType, bool isKing = false, bool isBadGuy = false)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+            IsKing = isKing;
+            IsBadGuy = isBadGuy;
+        }
+    }
+}
---
+++ week-07/day-3/BankOfSimba/BankOfSimba/Models/BankAccounts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccounts
+    {
+        public List<BankAccount> animals = new List<BankAccount>{
+            new BankAccount("Simba", "2000", "Lion", true, false),
+            new BankAccount("Pumbaa", "0", "Warthog"),
+            new BankAccount("Timon", "0", "Meerkat"),
+            new BankAccount("Rafiki", "1000", "Mandrill"),
+            new BankAccount("Nala", "1500", "Lioness")
+        };
+
+        public BankAccounts()
+        {
+        }
+    }
+}
