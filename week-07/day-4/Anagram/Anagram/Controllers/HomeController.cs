fc5a69b487e354ef18d635a1a312fc1a80e98b28
DeveloperADJ <developeradj@gmail.com>
2018-04-04 17:34:23 +0200

    Started the anagram exercise

---
+++ week-07/day-4/Anagram/Anagram/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Anagram.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anagram.Controllers
+{
+    public class HomeController : Controller
+    {
+        AnagramChecker anagramChecker = new AnagramChecker();
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ week-07/day-4/Anagram/Anagram/Models/AnagramChecker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagram.Models
+{
+    public class AnagramChecker
+    {
+        public string Message { get; set; }
+
+        public AnagramChecker(string message = "The word is not an anagram")
+        {
+            Message = message;
+        }
+
+        public static string CheckWordForAnagram(string word) => $"{word} is an Anagram.";
+    }
+}
