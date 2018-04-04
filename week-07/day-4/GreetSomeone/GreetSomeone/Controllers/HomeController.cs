2630b8116289f427665f11cb4defc71b2dce3b3d
DeveloperADJ <developeradj@gmail.com>
2018-04-04 17:14:21 +0200

    Did the basics for the GreetSomeone exercise, need to add dependency injection

---
+++ week-07/day-4/GreetSomeone/GreetSomeone/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GreetSomeone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreetSomeone.Controllers
+{
+    public class HomeController : Controller
+    {
+        static GreetingRobot greetingRobot = new GreetingRobot();
+
+        [Route("Index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(greetingRobot);
+        }
+
+        [Route("Greet")]
+        [HttpPost]
+        public IActionResult Greeter(string name)
+        {
+            greetingRobot.Message = "Welcome ";
+            greetingRobot.Name = name;
+            return View(greetingRobot);
+        }
+    }
+}
---
+++ week-07/day-4/GreetSomeone/GreetSomeone/Models/GreetingRobot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetSomeone.Models
+{
+    public class GreetingRobot
+    {
+        public string Name { get; set; }
+        public string Message { get; set; }
+
+        public GreetingRobot(string Name = "", string Message = "What is your name?")
+        {
+            this.Name = Name;
+            this.Message = Message;
+        }
+    }
+}
