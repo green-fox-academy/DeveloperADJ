c4eb48c89cafea8ce5c3e8f7239c52a2374c7f1b
DeveloperADJ <developeradj@gmail.com>
2018-04-19 21:00:58 +0200

    Finished Corsac Orientation Trial Exam

---
+++ week-09/day-4/CorsacExamTry2/CorsacExamTry2/Repositories/SpaceShipRepo.cs
@@ -0,0 +1,15 @@
+﻿using CorsacExamTry2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorsacExamTry2.Repositories
+{
+    public class SpaceShipRepo : ICrud
+    {
+        public SpaceShipRepo()
+        {
+        }
+    }
+}
